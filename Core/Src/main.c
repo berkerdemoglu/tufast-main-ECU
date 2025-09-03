9850f9241025b152d50cf54c6a5e176dc9693aae
Berk Erdemoglu <berkerdemoglu1120@gmail.com>
2025-09-03 23:21:04 +0200

    Reset CAN baud rate to 500 Kbps for now

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -422,9 +422,9 @@ static void MX_FDCAN1_Init(void)
     hfdcan1.Init.AutoRetransmission = DISABLE;
     hfdcan1.Init.TransmitPause = DISABLE;
     hfdcan1.Init.ProtocolException = DISABLE;
-    hfdcan1.Init.NominalPrescaler = 16;
+    hfdcan1.Init.NominalPrescaler = 8;
     hfdcan1.Init.NominalSyncJumpWidth = 1;
-    hfdcan1.Init.NominalTimeSeg1 = 13;
+    hfdcan1.Init.NominalTimeSeg1 = 29;
     hfdcan1.Init.NominalTimeSeg2 = 2;
     hfdcan1.Init.DataPrescaler = 1;
     hfdcan1.Init.DataSyncJumpWidth = 1;
