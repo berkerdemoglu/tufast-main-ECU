799ebc9861ea70d11ff0a305e2e5e343bde91e97
aswinraj222000 <63457374+aswinraj222000@users.noreply.github.com>
2025-10-10 15:54:14 +0200

    reduced CAN ID

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -54,16 +54,16 @@ extern "C" {
 #define PIN_ERROR   GPIO_PIN_6
 
 // --- Define IDs for A2C2 ---
-#define A2C2_ACC_X_ID          0x000C0B00
-#define A2C2_ACC_Y_ID          0x000C0C00
-#define A2C2_ACC_Z_ID          0x000C0D00
-#define A2C2_ANGV_X_ID         0x000C0E00
-#define A2C2_ANGV_Y_ID         0x000C0F00
-#define A2C2_ANGV_Z_ID         0x000C1000
-#define A2C2_BRAKE_TEMP_ID     0x000C1100
-#define A2C2_PRESSURE2_ID      0x000C1200
-#define A2C2_PRESSURE1_ID      0x000C1300
-#define A2C2_POTENTIOMETER_ID  0x000C1400
+#define A2C2_ACC_X_ID          0x000C0B
+#define A2C2_ACC_Y_ID          0x000C0C
+#define A2C2_ACC_Z_ID          0x000C0D
+#define A2C2_ANGV_X_ID         0x000C0E
+#define A2C2_ANGV_Y_ID         0x000C0F
+#define A2C2_ANGV_Z_ID         0x000C10
+#define A2C2_BRAKE_TEMP_ID     0x000C11
+#define A2C2_PRESSURE2_ID      0x000C12
+#define A2C2_PRESSURE1_ID      0x000C13
+#define A2C2_POTENTIOMETER_ID  0x000C14
 
 /* USER CODE END Includes */
 
